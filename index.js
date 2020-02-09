f752275381959f8aafd5c8abdd3cae628aae912d
William Kontos <williamjkontos@gmail.com>
2020-02-09 03:48:37 -0500

    removed the src folder for a direct path to index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime-types');
+const argv = require('minimist')(process.argv.slice(2));
+const {ingestReport} = require('./ingest_helpers');
+const {throwError, deleteReport} = require('./utils');
+
+let reportExists;
+const acceptedMimeTypes = [
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'text/plain; charset=utf-8',
+];
+
+if (argv.ingest) {
+  try {
+    const report = argv.ingest.includes('/') ? argv.ingest : path.join(__dirname, argv.ingest);
+    const mimeType = mime.contentType(path.extname(report));
+
+    if (!acceptedMimeTypes.includes(mimeType)) {
+      throwError({message: 'Error: Only .xlsx & .txt files can be ingested'});
+    }
+
+    const directory = './ingested_sales_reports';
+    const file = path.basename(report).split('.')[0];
+
+    reportExists = fs.existsSync(path.join(directory, `${file}.json`));
+
+    ingestReport(report, reportExists, directory, file);
+  } catch (error) {
+    // will rollback changes if any took place
+    // guards against files being deleted if error and no changes took place
+    if (reportExists && error.code !== 'ENOENT') {
+      deleteReport(directory, file);
+    }
+
+    throwError({message: error});
+  }
+} else if (argv.summary) {
+  console.log;
+}
