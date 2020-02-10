c39213a07a02ff24262c70e94445e64a63bbc5b7
William Kontos <wkontos@cc-wkontos-mbp.local>
2020-02-09 19:47:03 -0500

    document classes

--- utils/report-check.js
+++ utils/report-check.js
@@ -3,16 +3,38 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * A simple object that exposes validation methods for report operations
+ *
+ * @class ReportCheck
+ */
+
 class ReportCheck {
+
+  /** 
+   * @param directory - the directory of ingested files
+   * @param file - the current file being ingested
+   */
+
   constructor(directory, file = '') {
     this.directory = directory;
     this.file = file;
   }
 
+  /**
+   * Checks if a report by the given file name exists in the file-system
+   *
+   * @access public
+   */
   reportExists() {
     return fs.existsSync(path.join(this.directory, `${this.file}.json`));
   }
 
+  /**
+   * Checks if there are any reports in the file-system
+   *
+   * @access public
+   */
   reportsExistInDir() {
     let reportsExistInDir = false;
 
